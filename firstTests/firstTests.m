7bafb046d0f2a727d7392d7488205cead871e68d
tester2ltrm <tester2@email.ua>
2012-12-20 14:31:34 +0200

    no message

--- firstTests/firstTests.m
+++ firstTests/firstTests.m
@@ -27,23 +27,15 @@
     [super tearDown];
 }
 
-- (void)testExample
-{
-    
-    //STFail(@"Unit tests are not implemented yet in firstTests");
-}
-
 - (void)testThing
 {
     
-    int a = 1;
-    STAssertEquals(0, a , @"a_TestCase");
+    //int a = 1;
+   // STAssertEquals(0, a , @"a_TestCase");
     
     [test_viewcontroller handleTap:test_tap_recognizer];
     STAssertEquals(350, [test_viewcontroller handleTap:test_tap_recognizer] , @"Tap_TestCase");
-        //STAssertEquals(350, a , @"tqtq");
-        //            @"bad amount; 1.23 != %f",
-        //          [entry amount]);
+    
     NSLog(@"Log");
     //STFail(@"Unit tests are not implemented yet in firstTests");
 }
